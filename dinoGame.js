19325cd1725c891844002526731503b526a5e2a8
Mrakovsky <soutam716@gmail.com>
2021-08-25 21:23:45 +0900

    40m

--- dinoGame.js
+++ dinoGame.js
@@ -1,1 +1,9 @@
-console.log('こんにちは赤ちゃん')
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const imageNames = ['bird', 'cactus', 'dino'];
+
+const dinoImage = new Image(); // img 要素を作成
+dinoImage.src = `image/dino.png`; // 画像のパスを指定
+dinoImage.onload = () => {
+    ctx.drawImage(dinoImage, 0, 320); // 画像の描画
+}
